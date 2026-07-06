5987f01150e8a1c6d9f54568af23d71d5cf84889
ProfessorZ <ProfessorZ@users.noreply.github.com>
2026-07-06 17:33:54 +0100

    Update version to 1.4.0 in version.h

--- irFFB2026/version.h
+++ irFFB2026/version.h
@@ -17,7 +17,7 @@
 #define IRFFB_VER_FILEVERSION IRFFB_VER_MAJOR,IRFFB_VER_MINOR,IRFFB_VER_PATCH,IRFFB_VER_BUILD
 
 /* String forms (keep in sync with the numeric fields above). */
-#define IRFFB_VERSION_STR   "1.3.1"
-#define IRFFB_VERSION_WSTR  L"1.3.1"
+#define IRFFB_VERSION_STR   "1.4.0"
+#define IRFFB_VERSION_WSTR  L"1.4.0"
 
 #endif /* IRFFB_VERSION_H */
